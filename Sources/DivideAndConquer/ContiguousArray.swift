61c38948e777af545d4e4a94cfa859cde3d5b308
Dave Abrahams <dabrahams@google.com>
2020-04-05 16:55:08 -0700

    Use yield to return the slice

--- Sources/DivideAndConquer/ContiguousArray.swift
+++ Sources/DivideAndConquer/ContiguousArray.swift
@@ -425,14 +425,16 @@ extension ContiguousArray: RandomAccessCollection, MutableCollection {
       _checkIndex(bounds.upperBound)
       return ArraySlice(_buffer: _buffer[bounds])
     }
-    set(rhs) {
+    _modify {
       _checkIndex(bounds.lowerBound)
       _checkIndex(bounds.upperBound)
+      var y = ArraySlice(_buffer: _buffer[bounds])
+      yield &y
       // If the replacement buffer has same identity, and the ranges match,
       // then this was a pinned in-place modification, nothing further needed.
-      if self[bounds]._buffer.identity != rhs._buffer.identity
-      || bounds != rhs.startIndex..<rhs.endIndex {
-        self.replaceSubrange(bounds, with: rhs)
+      if self[bounds]._buffer.identity != y._buffer.identity
+      || bounds != y.startIndex..<y.endIndex {
+        self.replaceSubrange(bounds, with: y)
       }
     }
   }
