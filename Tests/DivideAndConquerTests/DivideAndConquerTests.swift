2f06df88d83a52fe34bd6699cf3f8497a4e8afb2
Dave Abrahams <dabrahams@google.com>
2020-04-05 22:03:22 -0700

    Clean up test code

--- Tests/DivideAndConquerTests/DivideAndConquerTests.swift
+++ Tests/DivideAndConquerTests/DivideAndConquerTests.swift
@@ -1,68 +1,58 @@
 import XCTest
-//@testable
 import DivideAndConquer
 
-func indent(_ depth: Int) -> String {
-  repeatElement(" ", count: depth).joined()
-}
-
 extension DivideAndConquer.ArraySlice {
-  mutating func scramble(
-      depth: Int, noCOW: ClosedRange<UnsafePointer<Element>>
-  ) -> Int {
-    var reallocations = 0
-    // print(indent(depth), "scramble(\(startIndex)..<\(endIndex))")
+  /// Scramble `self`, returning the number of reallocations due to COW, given
+  /// that the storage of `self` was expected to match `expectedFootprint`.
+  mutating func scramble(in expectedFootprint: ClosedRange<UnsafePointer<Element>>) -> Int {
     let base = withUnsafeBufferPointer { $0.baseAddress! }
-    if (!noCOW.contains(base) || !noCOW.contains(base+count)) {
-      reallocations = 1
-    }
+    var reallocations = expectedFootprint.contains(base) ? 0 : 1
     if count < 1 { return reallocations }
     if count < 4 {
       swapAt(startIndex, endIndex - 1)
     }
     else {
-      let noCOW = base...(base+count)
+      let footprint = base...(base+count)
       let m = (startIndex + endIndex) / 2
-      reallocations +=
-      self[startIndex..<m].scramble(depth: depth + 1, noCOW: noCOW)
-      reallocations +=
-      self[m..<endIndex].scramble(depth: depth + 1, noCOW: noCOW)
+      reallocations += self[startIndex..<m].scramble(in: footprint)
+      reallocations += self[m..<endIndex].scramble(in: footprint)
     }
     return reallocations
   }
 
+  /// Scramble `self`, returning the number of reallocations due to COW.
   mutating func scramble() -> Int {
     if count < 1 { return 0 }
     let base = withUnsafeBufferPointer { $0.baseAddress! }
-    let noCOW = base...base + count
+    let footprint = base...base + count
     if count < 4 {
       swapAt(startIndex, endIndex - 1)
       return 0
     }
     else {
       let m = (startIndex + endIndex) / 2
-      let r0 = self[startIndex..<m].scramble(depth: 0, noCOW: noCOW)
-      let r1 = self[m..<endIndex].scramble(depth: 0, noCOW: noCOW)
+      let r0 = self[startIndex..<m].scramble(in: footprint)
+      let r1 = self[m..<endIndex].scramble(in: footprint)
       return r0 + r1
     }
   }
 }
 
-
-extension DivideAndConquer.ContiguousArray {
+extension ContiguousArray {
+  /// Scramble `self`, returning the number of reallocations due to COW.
   mutating func scramble() -> Int {
     if count < 1 { return 0 }
     let base = withUnsafeBufferPointer { $0.baseAddress! }
-    let noCOW = base...base + count
+    let footprint = base...base + count
     if count < 4 {
       swapAt(startIndex, endIndex - 1)
       return 0
     }
     else {
       let m = (startIndex + endIndex) / 2
-      return
-          self[startIndex..<m].scramble(depth: 0, noCOW: noCOW)
-          + self[m..<endIndex].scramble(depth: 0, noCOW: noCOW)
+      let r0 = self[startIndex..<m].scramble(in: footprint)
+      let r1 = self[m..<endIndex].scramble(in: footprint)
+      return r0 + r1
     }
   }
 }
